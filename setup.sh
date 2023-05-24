9686a9d769f49b0c7f0b5e88c8287cf65c1c3cca
Joel Wetzell <jwetzell@cait.org>
2023-05-24 09:01:21 -0500

    update dotfiles and add setup script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+echo "installing nvm"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+echo "installing sdkman"
+curl -s "https://get.sdkman.io" | bash
+
+echo "linking .zshrc"
+if [ -f ~/.zshrc ]; then
+    echo ".zshrc exists removing"
+    rm ~/.zshrc
+fi
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+
+echo "linking .vimrc"
+if [ -f ~/.vimrc ]; then
+    echo ".vimrc exists removing"
+    rm ~/.vimrc
+fi
+ln -s ~/dotfiles/.vimrc ~/.vimrc
