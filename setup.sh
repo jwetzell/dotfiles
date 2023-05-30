52c2c5bd6bc2917511722a45b187f7545d60dbd2
Joel Wetzell <jwetzell@yahoo.com>
2023-05-30 08:17:37 -0500

    add ohmyzsh install

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 echo "installing sdkman"
 curl -s "https://get.sdkman.io" | bash
 
+echo "installing ohmyzsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 echo "linking .zshrc"
 if [ -f ~/.zshrc ]; then
     echo ".zshrc exists removing"
